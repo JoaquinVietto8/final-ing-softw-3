0f377edc75d4fa9eddd4c4b34b82d844843e9218
JoaquinVietto8 <80827980+JoaquinVietto8@users.noreply.github.com>
2024-02-21 17:03:34 -0300

    Update index.js

--- diario/src/componentes/Inicio/index.js
+++ diario/src/componentes/Inicio/index.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { ObjectInicio } from "./ObjectInicio";
 import './inicio.css';
 
+var vacio = false;
+
 export const Noticias = () => {
 
     const [noticias, setNoticias] = useState([])
