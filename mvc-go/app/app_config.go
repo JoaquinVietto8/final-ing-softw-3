5c9e7bd27ab572b50df20f5ca197fb84633b8990
JoaquinVietto8 <80827980+JoaquinVietto8@users.noreply.github.com>
2024-07-03 04:33:11 -0300

    Update app_config.go

--- mvc-go/app/app_config.go
+++ mvc-go/app/app_config.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"fmt"
+	"net/http"
 	"os"
 
 	log "github.com/sirupsen/logrus"
@@ -12,4 +14,18 @@ func init() {
 	log.SetLevel(log.DebugLevel)
 	log.Info("Starting logger system")
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, World!")
+	})
+
+	log.Infof("Server is listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	/*
+		log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	*/
 }
